8e41a4f5c07ad7d84d6deef389957a9c6f99d37c
Evgeniy Sedov <sedoff-online@yandex.ru>
2023-03-19 00:07:48 +0700

    adds yargs

--- bin/weather.js
+++ bin/weather.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+import process from 'node:process';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+  .usage('Usage: $0 [options]')
+  .help('help')
+  .alias('help', 'h')
+  .strict()
+  .option('lat', {
+    describe: 'Latitude',
+    type: 'number',
+  })
+  .option('lon', {
+    describe: 'Longitude',
+    type: 'number',
+  })
+  .option('mode', {
+    describe: 'Information output mode',
+    type: 'string',
+  })
+  .choices('mode', ['json', 'xml', 'html'])
+  .default('mode', 'json')
+  .demandOption(['lat', 'lon'], 'Please provide latitude and longitude')
+  .command('$0', 'get weather', () => { }, (argv) => {
+    console.log(argv);
+  })
+  .parse();
